d5da14caecc2c141b61b0f5818f169ba01e25589
Ralph Urlus <urlus.ralph@gmail.com>
2019-02-16 20:26:30 +0100

    temp set-up file to enable easy interaction with dev notebook

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='afinipy',
+      version='0.1',
+      description='Python utility functionality with data science orientationPersonal lib with utility functions',
+      author='Ralph Urlus',
+      author_email='rurlus.dev@gmail.com',
+      packages=['afinipy'],
+      zip_safe=True)
