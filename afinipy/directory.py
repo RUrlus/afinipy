c0489adfa640aff27f70ccb7acad4044bc977977
Ralph Urlus <urlus.ralph@gmail.com>
2019-02-16 20:27:33 +0100

    Class that stores and parses the modules in a directory

---
+++ afinipy/directory.py
@@ -0,0 +1,84 @@
+import os
+
+import afinipy.path_funcs as pf
+
+
+class Directory(object):
+    """Class that orchastrates the parsing of a directory in
+    the tree of the target directory
+    """
+    def __init__(self, path, level, parents, files, exclude=None):
+        """Initialise the class
+
+        Parameters
+        ----------
+        path : str
+            The path to the root of the directory
+        level : int
+            The level from the root directory
+        parents : str
+            The parent directories starting at the target path
+        files : list
+            The files in the directory
+        exclude : list-like
+            The modules to exclude
+        """
+        # get absolute path and check existence
+        self.root = pf.adir(path)
+
+        # name of the class is the directory name
+        self.name = pf.dir_name(self.root)
+
+        # the name(s) of the parent directories seperated by a dot
+        self.parents = parents
+
+        # the level from the target directory
+        self.level = level
+
+        # files contains all files found
+        # in the root directory
+        self.files = files
+
+        # the modules that should be excluded
+        self.exclude = exclude or set()
+        self.exclude.update({'__init__', 'setup'})
+        # files parser sets all the python modules found
+        # in the directory
+        self.modules = self._files_parser()
+
+    def _ismodule(self, f):
+        """Check if file is python module
+
+        Parameters
+        ----------
+        f : str
+            path to file
+
+        Returns
+        -------
+        bool
+            whether the file is a python module that is relevant
+        """
+        if os.path.isfile(os.path.join(self.root, f)):
+            name, ext = os.path.splitext(f)
+            return ((ext == '.py') and
+                    (name[0] not in {'.', '_'}) and
+                    (name not in (self.exclude)))
+        else:
+            return False
+
+    def _files_parser(self):
+        """Filter the python modules from the files and set as modules,
+        where we exclude:
+            - dotfiles
+            - private files, those starting with `_`
+            - __init__
+            _ setup
+
+        Returns
+        -------
+        set
+            All the python modules that fulfill the criteria
+        """
+
+        return set([os.path.splitext(f)[0] for f in self.files if self._ismodule(f)])
