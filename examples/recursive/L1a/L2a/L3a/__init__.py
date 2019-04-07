2994585116fa336b376c963cd433676f2780db2b
Ralph Urlus <urlus.ralph@gmail.com>
2019-04-07 02:09:09 +0200

    Updated examples

--- examples/recursive/L1a/L2a/L3a/__init__.py
+++
@@ -1,4 +0,0 @@
-from .tla import tl_a_func
-from .tla import TlAClass
-
-__all__ = ['tl_a_func', 'TlAClass']
--- examples/recursive/__init__.py
+++
@@ -1,5 +0,0 @@
-from .rootlevel import rl_func
-from .rootlevel import rl_func2
-from .rootlevel import RlClass
-
-__all__ = ['rl_func', 'rl_func2', 'RlClass']
