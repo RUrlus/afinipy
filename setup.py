9d0913e061fb607a55608eb46b38b274f971a675
Ralph Urlus <urlus.ralph@gmail.com>
2019-04-14 19:27:38 +0200

    Afinipy release v0.1.3 -- Python 2.7 support

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='afinipy',
-    version='0.1.2',
+    version='0.1.3',
     description='Automated init builder',
     author='Ralph Urlus',
     author_email='rurlus.dev@gmail.com',
@@ -14,4 +14,36 @@ setup(
         [console_scripts]
         afinipy=afinipy.afinipy:cli
     ''',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/RUrlus/afinipy/issues',
+        'Source': 'https://github.com/RUrlus/afinipy',
+    },
+    keywords='init development',
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: GPL License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        # These classifiers are *not* checked by 'pip install'. See instead
+        # 'python_requires' below.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
 )
